PraneethMerugu/Celly.jl

Implement Phase 13 transition oracle and evidence foundation

test/test_phase13_quality_gates.jl
@@ -1,0 +1,6 @@
+using Aqua
+
+@testset "Phase 13 PottsToolkit package-quality gates" begin
+    Aqua.test_all(PottsToolkit)
+    @test isempty(Test.detect_ambiguities(PottsToolkit; recursive = true))
+end

PraneethMerugu/CorePotts.jl

test/test_phase13_quality_gates.jl
@@ -1,0 +1,6 @@
+using Aqua
+
+@testset "Phase 13 CorePotts package-quality gates" begin
+    Aqua.test_all(CorePotts)
+    @test isempty(Test.detect_ambiguities(CorePotts; recursive = true))
+end