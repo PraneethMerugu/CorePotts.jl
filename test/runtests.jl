PraneethMerugu/Potts.jl

Consolidate repository around domain responsibilities

test/runtests.jl
@@ -5,14 +5,14 @@ using CorePotts
 @testset "PottsToolkit" begin
     include("test_level2_authoring.jl")
     include("test_level1_authoring.jl")
-    include("test_phase11_equivalence.jl")
-    include("test_phase11_fragments.jl")
-    include("test_phase11_inspection.jl")
-    include("test_phase11_inventory.jl")
-    include("test_phase11_macro_contract.jl")
-    include("test_phase12_5_tiled.jl")
-    include("test_phase13_contract_versions.jl")
-    include("test_phase13_quality_gates.jl")
-    include("test_phase14_generic_fragments.jl")
+    include("test_authoring_equivalence.jl")
+    include("test_model_fragments.jl")
+    include("test_model_inspection.jl")
+    include("test_component_inventory.jl")
+    include("test_authoring_macro_contract.jl")
+    include("test_tiled_authoring.jl")
+    include("test_contract_versions.jl")
+    include("test_package_quality.jl")
+    include("test_generic_model_fragments.jl")
     include("test_documentation_quality_checker.jl")
 end

PraneethMerugu/CorePotts.jl

test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using CorePotts
 
 @testset "CorePotts" begin
+    include("support/algorithm_contracts.jl")
     include("test_logical_initialization.jl")
     include("test_logical_lifecycle.jl")
     include("test_logical_state.jl")
@@ -9,13 +10,13 @@ using CorePotts
     include("test_execution_contracts.jl")
     include("test_reference_engine.jl")
     include("test_scientific_protocols.jl")
-    include("test_phase13_contract_versions.jl")
-    include("test_phase13_quality_gates.jl")
+    include("test_contract_versions.jl")
+    include("test_package_quality.jl")
     include("test_state_semantics.jl")
-    include("test_phase8_protocols.jl")
-    include("test_phase8_lifecycle.jl")
-    include("test_phase8_persistence.jl")
-    include("test_phase9_sciml_interface.jl")
+    include("test_open_protocols.jl")
+    include("test_lifecycle_events.jl")
+    include("test_persistence.jl")
+    include("test_sciml_interface.jl")
     include("test_topology_abstractions.jl")
     include("test_cartesian_relations.jl")
     include("test_proposal_acceptance.jl")
@@ -26,22 +27,22 @@ using CorePotts
     include("test_scientific_focal_points.jl")
     include("test_scientific_elongation.jl")
     include("test_scientific_inner_loop.jl")
-    include("test_phase10_extension_protocol.jl")
+    include("test_extension_protocol.jl")
     include("test_scientific_mechanics.jl")
     include("test_sequential_algorithms.jl")
-    include("test_phase14_dynamic_state.jl")
-    include("test_phase14_relationship_processes.jl")
-    include("test_phase14_contact_relationships.jl")
-include("test_phase14_polarity_force.jl")
-include("test_phase14_vector_boundary_potential.jl")
-include("test_phase14_bounded_observations.jl")
-    include("test_phase14_cell_history.jl")
-include("test_phase14_history_polarity.jl")
-include("test_phase16_native_fields.jl")
-    include("test_phase16_process_bigraph_adapter.jl")
-    include("test_phase16_solver_plurality.jl")
-    include("test_phase16_merks2006.jl")
-    include("test_phase16_shirinifard2012.jl")
+    include("test_dynamic_state.jl")
+    include("test_relationship_processes.jl")
+    include("test_contact_relationships.jl")
+    include("test_polarity_force.jl")
+    include("test_vector_boundary_potential.jl")
+    include("test_bounded_observations.jl")
+    include("test_cell_history.jl")
+    include("test_history_polarity.jl")
+    include("test_native_fields.jl")
+    include("test_process_bigraph_adapter.jl")
+    include("test_solver_adapter_contract.jl")
+    include("test_merks2006_model.jl")
+    include("test_shirinifard2012_model.jl")
     include("test_checkerboard_algorithms.jl")
     include("test_tiled_checkerboard.jl")
     include("test_lottery_algorithms.jl")